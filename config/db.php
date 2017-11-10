35dcf17ced75f8eb10156a69a69e7f9a4ebd8694
Ivan <fedddd@inbox.ru>
2017-11-10 17:38:49 +0300

    add config

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+class db{
+
+/*
+ * Параметры подключения к базе данных
+ */
+public $host = "127.0.0.1";
+public $base = "calendar";
+public $user = "root";
+public $base_parol = "";
+
+
+
+}
+
+
+
+
+
